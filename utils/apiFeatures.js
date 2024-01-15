806e1e979387e24e53738bd4ec0ab8658308a2bf
PetarZerajic <petarzerajic99@gmail.com>
2024-01-15 14:35:44 +0100

    Adding eq operator

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -1,5 +1,4 @@
 class ApiFeatures {
-  //query pripada mongusa, a queryString dobijamo od ekspresa
   constructor(query, queryString) {
     this.query = query;
     this.queryString = queryString;
@@ -11,17 +10,15 @@ class ApiFeatures {
     excludedFields.forEach((el) => delete queryObj[el]);
 
     let queryStr = JSON.stringify(queryObj);
-    const pattern = /\b(gte|gt|lte|lt)\b/g; //Zelimo da uskladimo reci.Bez g bi samo uzeo prvu rec
-    queryStr = queryStr.replace(pattern, (match) => `$${match}`); //Podudaramo reci sa obrascem i dodajemo mu $ znak
+    const pattern = /\b(gte|gt|lte|lt|eq)\b/g;
+    queryStr = queryStr.replace(pattern, (match) => `$${match}`);
 
     this.query = this.query.find(JSON.parse(queryStr));
-
-    return this; //Odnosi se na instancu objekta na kojoj se metoda trenutno poziva. Koristi se za ulančavanje
+    return this;
   }
 
   sort() {
     if (this.queryString.sort) {
-      console.log(this.queryString);
       const sortBy = this.queryString.sort.split(",").join(" ");
       this.query = this.query.sort(sortBy);
     } else {
@@ -35,7 +32,7 @@ class ApiFeatures {
       const fields = this.queryString.fields.split(",").join(" ");
       this.query = this.query.select(fields);
     } else {
-      this.query = this.query.select("-__v"); // - ovde oznacava da ce polje biti iskljuceno
+      this.query = this.query.select("-__v");
     }
 
     return this;
