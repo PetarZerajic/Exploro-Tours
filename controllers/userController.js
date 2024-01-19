1ed8fffe2e964699705576dcfa76118e66c5fac6
PetarZerajic <petarzerajic99@gmail.com>
2024-01-19 12:04:57 +0100

    Updated userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -52,19 +52,16 @@ const getAllUsers = getAll(User);
 const getUser = getOne(User);
 
 const getMe = (req, res, next) => {
-  //Id iz parametra se dobija kada se korisnik uloguje.
   req.params.id = req.user.id;
-
   next();
 };
 
-//Samo za admina .NE azurirati sifre sa ovim
 const updateUser = updateOne(User);
 
 const updateMe = async (req, res, next) => {
   try {
     const { password, confirmPassword } = req.body;
-    //1) Create error if user POSTS password data
+
     if (password || confirmPassword) {
       return next(
         new AppError(
@@ -73,16 +70,16 @@ const updateMe = async (req, res, next) => {
         )
       );
     }
-    //2) Filtered out unwanted fields name that are not allowed to be updated
     const filteredBody = filterObj(req.body, "name", "email");
+
     if (req.file) filteredBody.photo = req.file.filename;
-    //3) Update user document
+
     const updatedUser = await User.findByIdAndUpdate(
       req.user.id,
       filteredBody,
       {
         new: true,
-        runValidators: true, //U slucaju da postavimo nevazecu adresu ona bi trebala biti uhvacena od strane validatora
+        runValidators: true,
       }
     );
 
@@ -99,7 +96,6 @@ const updateMe = async (req, res, next) => {
 
 const deleteMe = async (req, res, next) => {
   try {
-    //Cilje je da ih oznacimo kao neaktivne a ne da ih izbirisemo iz BP (primer ako neko hoce da vrati svoj nalog)
     await User.findByIdAndUpdate(req.user.id, { active: false });
 
     res.status(204).json({
@@ -111,7 +107,6 @@ const deleteMe = async (req, res, next) => {
   }
 };
 
-//Samo za admina
 const deleteUser = deleteOne(User);
 
 module.exports = {
